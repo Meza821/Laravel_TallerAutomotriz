02ebf5546b68eb59200766c4a9e8b32b51b7ecca
josue_romero <jescobar@teamxyon.com>
2025-05-09 22:46:05 -0600

    Modificaciones de Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
 
             // Seeder para Departamentos, distritos, usuarios y clientes.
-            DepartamentosSeeder::class,
-            DistritosSeeder::class,
-            UserSeeder::class,
+            //DepartamentosSeeder::class
+            //DistritosSeeder::class
             ClientSeeder::class
         ]);
       
